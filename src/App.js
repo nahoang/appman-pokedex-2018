c78564c82346260a5c48047989b41e4d5bd7cd85
nahoang9698 <nahoang9698@gmail.com>
2019-11-29 13:01:50 +0700

    feat(App): add home page
    add library style-components
    add footer and button add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import styled from 'styled-components'
 
 const COLORS = {
   Psychic: "#f8a5c2",
@@ -15,11 +16,55 @@ const COLORS = {
   Fire: "#eb4d4b"
 }
 
+
+
+const Wrapper = styled.div`
+  heigth: 100%
+`
+
+const Footer = styled.div`
+  position: absolute;  
+  height: 100px;
+  background-color: ${COLORS.Fire};
+  width: 100%;
+  bottom: 0;
+  opacity: 0.9;
+`
+
+const StyledButton = styled.div`
+  background-color: ${COLORS.Fire};
+  position: absolute;
+  bottom: 0;
+  width: 150px;
+  height: 150px;
+  border-radius: 100px;
+  left: 0;
+  right: 0;
+  margin-right: auto;
+  margin-left: auto
+  text-align: center;
+  p {
+    font-size: 100px;
+    color: #ffffff;
+    margin: 0 auto;
+  }
+`
+
 class App extends Component {
+
+  openModal() {
+    
+  }
+
   render() {
     return (
-      <div className="App">
-      </div>
+      <Wrapper className="App">
+        <Footer>
+          <StyledButton onClick={this.openModal.bind(this)}>
+              <p>+</p>
+          </StyledButton>
+        </Footer>
+      </Wrapper>
     )
   }
 }
